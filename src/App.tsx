16aaad54018f7258a116253dacb602fbf2be7c2e
YAMITRAO <work.aky@gmail.com>
2025-01-21 17:37:06 +0530

    code ready for ci/cd

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,48 @@
 import "./App.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  BrowserRouter,
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/home/Home";
 import Setting from "./pages/setting/Setting";
 import ProjCompListDesign from "./components/proj_comp/list_design/ProjCompListDesign";
 
 function App() {
-  const router = createBrowserRouter([
+  const router = createBrowserRouter(
+    [
+      {
+        path: "/",
+        element: <Layout />,
+        children: [
+          {
+            path: "/",
+            element: <Home />,
+          },
+          {
+            path: "/setting",
+            element: <Setting />,
+          },
+          {
+            path: "/test",
+            element: <ProjCompListDesign />,
+          },
+        ],
+      },
+      {
+        path: "*",
+        element: (
+          <div className="bg-slate-300 dark:bg-slate-700 h-screen dark:text-slate-200 flex justify-center items-center">
+            Error Page here
+          </div>
+        ),
+      },
+    ],
     {
-      path: "",
-      element: <Layout />,
-      children: [
-        {
-          path: "/",
-          element: <Home />,
-        },
-        {
-          path: "/setting",
-          element: <Setting />,
-        },
-        {
-          path: "/test",
-          element: <ProjCompListDesign />,
-        },
-      ],
-    },
-  ]);
+      basename: "/common_test_web",
+    }
+  );
 
   // handle dark and normal mode
   const handleBackground = () => {
@@ -37,12 +54,14 @@ function App() {
   };
   return (
     <div className="w-screen h-screen">
+      {/* <ProjCompListDesign /> */}
       <button
         className="fixed top-2 z-50 right-2 bg-green-700 px-2 rounded-lg text-slate-200"
         onClick={handleBackground}
       >
         Switch
       </button>
+
       <RouterProvider router={router} />
     </div>
   );
