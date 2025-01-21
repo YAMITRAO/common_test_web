4812e4e7e032c9efc0bacd5597b4bafb300bbc0a
YAMITRAO <work.aky@gmail.com>
2025-01-21 15:22:45 +0530

    git config changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/common_test_web/", // git repo for github page to live
   plugins: [react()],
-})
+  build: {
+    outDir: "dist", // The build output folder (default is "dist")
+  },
+});
